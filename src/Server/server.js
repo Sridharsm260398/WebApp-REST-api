ad43a529cdc7fdabe5781ac6518b44acdcbb5a9e
Sridharsm260398 <sridharsmwork@gmail.com>
2024-06-06 17:01:51 +0530

    Update server.js

--- src/Server/server.js
+++ src/Server/server.js
@@ -2,7 +2,7 @@ const app = require('../app');
 const dotenv = require('dotenv');
 dotenv.config({ path: './src/config.env' })
 //console.log(process.env.NODE_ENV);
-const port = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Listening : http://localhost:${PORT}`);
 });
